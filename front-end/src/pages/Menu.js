93774746c3fab700be83f5c46b5320baea4d091d
UMDstudent2024 <hzalzali@umich.edu>
2023-10-21 19:50:51 -0400

    next update

--- front-end/src/pages/Menu.js
+++ front-end/src/pages/Menu.js
@@ -1,4 +1,18 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 function Menu() {
+  const location = useLocation();
+  const [text, setText] = useState(location.state.text);
+  const [type, setType] = useState(location.state.type);
+  const [file, setFile] = useState(location.state.file);
+  const [avatar, setAvatar] = useState(0);
+  const [template, setTemplate] = useState(0);
+  useEffect(() => {
+    console.log(location.state);
+    console.log(text);
+    console.log(type);
+    console.log(file);
+  }, [location.state]);
   return (
     <div className="App">
       <p>Menu Page</p>
