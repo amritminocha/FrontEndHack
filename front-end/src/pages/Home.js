b867524b18ca496967e85cfb0b77e3839606fca6
UMDstudent2024 <hzalzali@umich.edu>
2023-10-21 14:51:58 -0400

    works on home page

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,9 +1,11 @@
+import "./css/Home.css";
 function Home() {
   return (
-    <div className="App">
-      <p>Home</p>
+    <div className="Background">
+      <div className="container text-white p-5">
+      <h1 className = "Title w-75">Craft Your Presentation in Seconds with AI</h1>
+      </div>
     </div>
   );
 }
-
 export default Home;
