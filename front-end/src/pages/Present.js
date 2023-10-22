693974b7539df93021455e488774b28e177dbb92
UMDstudent2024 <hzalzali@umich.edu>
2023-10-22 00:44:06 -0400

    car finally works

--- front-end/src/pages/Present.js
+++ front-end/src/pages/Present.js
@@ -1,10 +1,45 @@
 import Carousel from "../components/Carousel";
 import "./css/Present.css";
+import template1 from "../assets/templates/template1 (1).png";
+import template2 from "../assets/templates/template1 (2).png";
+import template3 from "../assets/templates/template1 (3).png";
+import music1 from "../assets/audio_samples/music1.mp3";
+import music2 from "../assets/audio_samples/music2.mp3";
+import music3 from "../assets/audio_samples/music3.mp3";
 function Present() {
+  const simulatedSlides = [
+    {
+      src: template1,
+      alt: "Slide 1",
+    },
+    {
+      src: template2,
+      alt: "Slide 2",
+    },
+    {
+      src: template3,
+      alt: "Slide 3",
+    },
+  ];
+  const simulatedAudio = [
+    {
+      src: music1,
+      alt: "Music 1",
+    },
+    {
+      src: music2,
+      alt: "Music 2",
+    },
+    {
+      src: music3,
+      alt: "Music 3",
+    },
+  ];
+  const audioRefs = simulatedAudio.map((prop) => new Audio(prop.src));
   return (
     <div className="Background3">
       <p>Present Page</p>
-      <Carousel />
+      <Carousel slide={simulatedSlides} audioRefs={audioRefs} />
     </div>
   );
 }
