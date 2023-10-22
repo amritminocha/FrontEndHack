69d54b783e3bd4bd8000ee20913ace494a85c779
UMDstudent2024 <hzalzali@umich.edu>
2023-10-22 03:25:07 -0400

    made loading page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
-import Home from './pages/Home';
-import Menu from './pages/Menu';
-import Present from './pages/Present';
-import ErrorPage from './pages/404';
-import {Routes, Route} from 'react-router-dom';
+import "./App.css";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Present from "./pages/Present";
+import Loading from "./pages/Loading";
+import ErrorPage from "./pages/404";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element = {<Home/>}/>
-        <Route path="/menu" element = {<Menu/>}/>
-        <Route path="/present" element = {<Present/>}/>
-        <Route path="*" element = {<ErrorPage/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/loading" element={<Loading />} />
+        <Route path="/present" element={<Present />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
   );
---
+++ front-end/src/pages/Loading.js
@@ -0,0 +1,10 @@
+import "./css/Menu.css";
+function Loading() {
+  return (
+    <div className="Background3">
+      <p>Loading Page</p>
+    </div>
+  );
+}
+
+export default Loading;
